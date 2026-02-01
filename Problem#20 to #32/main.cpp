56f317e92115ccf87cb6413cf1050abc3a76ed39
JinxDevs <JinxDevs@JINX>
2026-02-01 03:04:44 -0800

    add

--- Problem#20 to #32/main.cpp
+++ Problem#20 to #32/main.cpp
@@ -3,16 +3,17 @@
 //IncreaseDateByXDays -- Done
 //IncreaseDateByOneWeek -- Done
 //IncreaseDateByXWeeks -- Done
-//IncreaseDateByOneMonth
-//IncreaseDateByXMonth
-//IncreaseDateByOneYear
-//IncreaseDateByXYears
-//IncreaseDateByXYearsFaster
-//IncreaseDateByOneDecade
-//IncreaseDateByXDecades
-//IncreaseDateByXDecadesFaster
-//IncreaseDateByOneCentury
-//IncreaseDateByOneMillennium
+//IncreaseDateByOneMonth -- Done
+//IncreaseDateByXMonth -- Done
+//IncreaseDateByOneYear -- Done
+//IncreaseDateByXYears -- Done
+//IncreaseDateByXYearsFaster -- Done
+//IncreaseDateByOneDecade -- Done
+//IncreaseDateByXDecades -- Done
+//IncreaseDateByXDecadesFaster -- Done
+//IncreaseDateByOneCentury -- Done
+//IncreaseDateByOneMillennium -- Done
+
 
 #include<iostream>
 #include<string>
@@ -99,21 +100,84 @@ void IncreaseDateByOneMonth(stDate& Date) {
 			Date.Day = DaysInAMonth(Date);
 		}
 	}
+}
 
+void IncreaseDateByXMonths(stDate& Date, int MonthsToIncrease) {
+
+	for (int i = 0; i < MonthsToIncrease; i++) {
+
+		IncreaseDateByOneMonth(Date);
+	}
+}
+
+
+void IncreaseDateByOneYear(stDate& Date) {
+
+	Date.Year++;
+	if (Date.Month == 2 && Date.Day > DaysInAMonth(Date)) {
+		Date.Day = DaysInAMonth(Date);
+	}
+	
+}
+
+void IncreaseDateByXYears(stDate& Date, int YearsToIncrease) {
+
+	for (short i = 0; i < YearsToIncrease; i++) {
+		IncreaseDateByOneYear(Date);
+	}
 
 }
+
+void IncreaseDateByXYearsFaster(stDate& Date, int YearsToIncrease) {
+	Date.Year += YearsToIncrease;
+	if (Date.Month == 2 && Date.Day > DaysInAMonth(Date)) {
+		Date.Day = DaysInAMonth(Date);
+	}
+}
+
+void IncreaseDateByOneDecade(stDate& Date) {
+
+	IncreaseDateByXYearsFaster(Date, 10);
+
+}
+
+void IncreaseDateByXDecades(stDate& Date, int DecadesToIncrease) {
+
+	for (int i = 0; i < DecadesToIncrease; i++) {
+		IncreaseDateByOneDecade(Date);
+	}
+
+}
+
+void IncreaseDateByXDecadesFaster(stDate& Date, int DecadesToIncrease) {
+
+	Date.Year += DecadesToIncrease * 10;
+	if (Date.Month == 2 && Date.Day > DaysInAMonth(Date)) {
+		Date.Day = DaysInAMonth(Date);
+	}
+}
+
+
+void IncreaseDateByOneCentury(stDate& Date) {
+	IncreaseDateByXDecadesFaster(Date, 10);
+}
+
+void IncreaseDateByOneMillennium(stDate& Date) {
+
+	IncreaseDateByXDecadesFaster(Date, 100);
+}
+
 string PrintDate(const stDate& Date) {
-
-
 
 	return to_string(Date.Day)+"/"+to_string(Date.Month)+"/"+to_string(Date.Year);
 
 }
 
+
 int main() {
 
 	stDate Date;
-	Date.Year = 2025;
+	Date.Year = 2019;
 	Date.Month = 1;
 	Date.Day = 1;
 	string line;
@@ -126,6 +190,27 @@ int main() {
 	cout << "Date after increasing one week: " << PrintDate(Date) << endl;
 	IncreaseDateByXWeeks(Date, 3);
 	cout << "Date after increasing 3 weeks: " << PrintDate(Date) << endl;
+	IncreaseDateByOneMonth(Date);
+	cout << "Date after increasing one month: " << PrintDate(Date) << endl;
+	IncreaseDateByXMonths(Date, 2);
+	cout << "Date after increasing 2 months: " << PrintDate(Date) << endl;
+	IncreaseDateByOneYear(Date);
+	cout << "Date after increasing one year: " << PrintDate(Date) << endl;
+	IncreaseDateByXYears(Date, 2);
+	cout << "Date after increasing two years: " << PrintDate(Date) << endl;
+	IncreaseDateByXYearsFaster(Date, 2);
+	cout << "Date after increasing two years (faster): " << PrintDate(Date) << endl;
+	IncreaseDateByOneDecade(Date);
+	cout << "Date after increasing one decade: " << PrintDate(Date) << endl;
+	IncreaseDateByXDecades(Date, 2);
+	cout << "Date after increasing two decades: " << PrintDate(Date) << endl;
+	IncreaseDateByXDecadesFaster(Date, 2);
+	cout << "Date after increasing two decades (faster): " << PrintDate(Date) << endl;
+	IncreaseDateByOneCentury(Date);
+	cout << "Date after increasing one century: " << PrintDate(Date) << endl;
+	IncreaseDateByOneMillennium(Date);
+	cout << "Date after increasing one millenium: " << PrintDate(Date) << endl;
+
 
 	return 0;
 }
